85618b77f73402a15519d3c27cd066d467b76dbc
Udit Kundu <uditkundu19@gmail.com>
2026-05-01 13:18:00 +0530

    added depth, next stuff will be concurrency

--- main.go
+++ main.go
@@ -6,7 +6,15 @@ import (
 	"net/http"
 )
 
-func crawl(url string) error {
+var urlTable = make(map[string][]string)
+var visitedUrls = make(map[string]bool)
+
+func crawl(url string, depth int) error {
+	if visitedUrls[url] {
+		return nil
+	}
+	visitedUrls[url] = true // updating first as if err occured i want to keep it as visited marked
+
 	resp, err := http.Get(url)
 	if err != nil {
 		return fmt.Errorf("fetching url %s: %w", url, err)
@@ -15,20 +23,43 @@ func crawl(url string) error {
 	// close the reader to prevent from resource leaks
 	defer resp.Body.Close()
 
-	urls, err := ParseUrlsFromHTML(resp.Body)
+	urls, err := ParseUrlsFromHTML(resp.Body, url)
+	// we are not handling the base url when our get request is gonna redirect
+	// there we have to use resp.Request.URL (that's the redirected url)
+	// we have to use that as a base url or else same seed hostname condition will break
 
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(len(urls))
-	fmt.Println(urls)
+	if depth > 0 {
+		for i := range urls {
+			crawl(urls[i], depth-1)
+		}
+	}
 
+	urlTable[url] = urls
 	return nil
 }
+
 func main() {
-	err := crawl("https://news.ycombinator.com")
+	url := "https://info.cern.ch/"
+	// url := "https://news.ycombinator.com/"
+	depth := 1
+	err := crawl(url, depth)
 	if err != nil {
 		log.Fatal(err)
 	}
+	printUrls()
+}
+
+func printUrls() {
+	for key, v := range urlTable {
+		fmt.Printf("KEY %s ----\n ", key)
+		for i := range v {
+			fmt.Println(v[i])
+		}
+		fmt.Printf("\n\n")
+	}
+	fmt.Println(visitedUrls, len(visitedUrls))
 }
--- parser.go
+++ parser.go
@@ -3,14 +3,21 @@ package main
 import (
 	"fmt"
 	"io"
+	"net/url"
 
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 )
 
 // Getting a Reader (html) and returning all the urls in the page in order as a slice. Urls are raw not parsed
-func ParseUrlsFromHTML(r io.Reader) ([]string, error) {
+func ParseUrlsFromHTML(r io.Reader, u string) ([]string, error) {
 	var urls []string
+	baseUrl, err := url.Parse(u)
+
+	if err != nil {
+		return nil, fmt.Errorf("parsing url %s: %w", u, err)
+	}
+
 	doc, err := html.Parse(r)
 
 	if err != nil {
@@ -21,29 +28,19 @@ func ParseUrlsFromHTML(r io.Reader) ([]string, error) {
 		if n.Type == html.ElementNode && n.DataAtom == atom.A {
 			for _, a := range n.Attr {
 				if a.Key == "href" {
-					urls = append(urls, a.Val)
-					break
-				}
-			}
-		}
-	}
+					parsedUrl, err := url.Parse(a.Val)
+					if err != nil {
+						continue
+					}
 
-	return urls, nil
-}
+					if !parsedUrl.IsAbs() {
+						// realive url
+						parsedUrl = baseUrl.ResolveReference(parsedUrl)
+					}
 
-// <a href="<url>"> ....
-func ParseUrlsFromHTMLMannual(html string) []string {
-	var urls []string
-
-	// outer loop
-	for i := 0; i < len(html)-2; i++ {
-		if html[i] == '<' && html[i+1] == 'a' && isTagEnd(html[i+2]) {
-			// inner loop after finding a tag
-			for j := i + 3; j < len(html); j++ {
-				if html[j] == '>' {
-					url := parseUrlFromAnchorTag(html[i : j+1]) // it will give <a>...href="">
-					if url != "" {
-						urls = append(urls, url)
+					if parsedUrl.Host == baseUrl.Host && (parsedUrl.Scheme == "https" || parsedUrl.Scheme == "http") {
+						parsedUrl.Fragment = ""
+						urls = append(urls, parsedUrl.String())
 					}
 					break
 				}
@@ -51,27 +48,5 @@ func ParseUrlsFromHTMLMannual(html string) []string {
 		}
 	}
 
-	return urls
-}
-
-func isTagEnd(b byte) bool {
-	return b == ' ' || b == '\t' || b == '\n' || b == '>' || b == '/'
-}
-
-func parseUrlFromAnchorTag(tag string) string {
-
-	for i := 0; i < len(tag)-5; i++ {
-		if tag[i] == 'h' && tag[i+1] == 'r' && tag[i+2] == 'e' && tag[i+3] == 'f' && tag[i+4] == '=' && tag[i+5] == '"' {
-			// url := strings.Builder{}
-			start := i + 6
-			for j := start; j < len(tag); j++ {
-				if tag[j] == '"' {
-					return tag[start:j]
-
-				}
-			}
-		}
-	}
-
-	return ""
+	return urls, nil
 }
