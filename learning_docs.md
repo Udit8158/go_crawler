7a5f4cdc401e6f32cc614b08a81000088af26518
Udit Kundu <uditkundu19@gmail.com>
2026-05-02 10:05:43 +0530

    concurrently fetching urls done - first version. there are problems like no limits yet

--- learning_docs.md
+++ learning_docs.md
@@ -24,3 +24,14 @@
 
 - Why defer close request body?
   - TCP scoket, file descriptor, buffer in memory vs live network connection
+- Why multiple go routines writting to an slice/map is bad but for channels it's good?
+
+# Notes
+
+- **Go routines with wg**
+  - spawn go rotuines add wg.Add for each routine and call wg.Done for each. 
+  - then before printing the final result add wg.Wait() so that it waits for each wg.Done() call 
+  - now when the go routine function will write data in a data strcuture like array, maps we need to use wg.Mutex
+  - why? because of **atomicity**. if multiple thing writes a ds in same time - they might read the value same and then update that value (so basically even after multiple writes the value only can change by one). and compiler doesn't allow that ofc.
+  - also there are problems with maps for resizing and all (not clear to me yet)
+  - that's the reason we have to use mutex.Lock before reading/writting a ds and then unlock that. that's how atomicity will remain. and we can test that from go run -race (command to test *race condition - where mutiple threads write to same location*)
--- main.go
+++ main.go
@@ -4,16 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
 
 var urlTable = make(map[string][]string)
 var visitedUrls = make(map[string]bool)
 
-func crawl(url string, depth int) error {
+func crawl(url string, depth int, wg *sync.WaitGroup, mu *sync.Mutex) error {
+	mu.Lock()
 	if visitedUrls[url] {
+		mu.Unlock()
 		return nil
 	}
 	visitedUrls[url] = true // updating first as if err occured i want to keep it as visited marked
+	mu.Unlock()
 
 	resp, err := http.Get(url)
 	if err != nil {
@@ -33,23 +37,34 @@ func crawl(url string, depth int) error {
 	}
 
 	if depth > 0 {
-		for i := range urls {
-			crawl(urls[i], depth-1)
+		for _, u := range urls {
+			wg.Add(1)
+			go func(u string) {
+				defer wg.Done()
+				crawl(u, depth-1, wg, mu)
+			}(u)
 		}
 	}
 
+	mu.Lock()
 	urlTable[url] = urls
+	mu.Unlock()
+
 	return nil
 }
 
 func main() {
 	url := "https://info.cern.ch/"
 	// url := "https://news.ycombinator.com/"
-	depth := 1
-	err := crawl(url, depth)
+	depth := 10
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	err := crawl(url, depth, &wg, &mu)
 	if err != nil {
 		log.Fatal(err)
 	}
+	wg.Wait()
 	printUrls()
 }
 
